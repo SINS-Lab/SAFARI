095eeefbc758198e2c4fd8ceae8509a2ba59ea62
prjohns <40612672+prjohns@users.noreply.github.com>
2019-02-26 16:07:43 -0500

    Post processing for xyz files
    this merges the xyz output from safari so that the lattice sites do not go poof whenever they leave the simulation distance

---
+++ misc/xyz_postprocess.py
@@ -0,0 +1,134 @@
+from xyz import XYZ
+from xyz import XYZ_Single
+
+class Particle:
+    def __init__(self):
+        self.atom = None
+        self.pos = [0,0,0]
+        self.momentum = [0,0,0]
+        self.velocity = [0,0,0]
+        self.mass = 0
+
+    def fromXYZ(self, atom, value):
+        self.atom = atom
+        self.pos = [value[0], value[1], value[2]]
+        self.momentum = [value[3], value[4], value[5]]
+        self.mass = value[6]
+        self.velocity = [value[3]/value[6], \
+                         value[4]/value[6], \
+                         value[5]/value[6]]
+        
+class ParticlePath:
+    def __init__(self):
+        self.particles=[]
+        
+    
+
+def isSame(p_a, p_b, dt):
+    if p_a == p_b:
+        return True
+    if p_a.atom != p_b.atom:
+        return False
+    #always the same for the projectile Atom
+    if p_a.atom == 'A':
+        return True
+    
+    p_a_dt = [p_a.pos[0] + p_a.velocity[0]*dt,\
+              p_a.pos[1] + p_a.velocity[1]*dt,\
+              p_a.pos[2] + p_a.velocity[2]*dt]
+    
+    p_b_dt = [p_b.pos[0] + p_b.velocity[0]*dt,\
+              p_b.pos[1] + p_b.velocity[1]*dt,\
+              p_b.pos[2] + p_b.velocity[2]*dt]
+    
+    diff = (p_a_dt[0] - p_b_dt[0])**2+\
+           (p_a_dt[1] - p_b_dt[1])**2+\
+           (p_a_dt[2] - p_b_dt[2])**2
+    return diff <= 1e-0
+
+def merge(pset1, pset2, dt):
+    len1 = len(pset1)
+    len2 = len(pset2)
+    merged = False
+    num = 0
+    for i in range(len1):
+        p_a = pset1[i]
+        has = False
+        for j in range(len2):
+            p_b = pset2[j]
+            if isSame(p_a, p_b, dt):
+                has = True
+                break
+        if not has:
+            pset2.append(p_a)
+            merged = True
+            num = num + 1
+    return merged, num
+
+def xyzFromParticles(time, pset):
+    xyz_single = XYZ_Single()
+    xyz_single.number = len(pset)
+    xyz_single.comment = str(time)+'\n'
+    for particle in pset:
+        xyz_single.atoms.append(particle.atom)
+        value = []
+        value.append(particle.pos[0])
+        value.append(particle.pos[1])
+        value.append(particle.pos[2])
+        value.append(particle.momentum[0])
+        value.append(particle.momentum[1])
+        value.append(particle.momentum[2])
+        value.append(particle.mass)
+        xyz_single.values.append(value)
+    return xyz_single
+
+def process(xyz):
+    times = []
+    particles = []
+    n = 0
+    for xyz_single in xyz.xyzs:
+        pset = []
+        times.append(float(xyz_single.comment))
+        for i in range(xyz_single.number):
+            particle = Particle()
+            particle.fromXYZ(xyz_single.atoms[i],xyz_single.values[i])
+            pset.append(particle)
+            n = n + 1
+        particles.append(pset)
+    merged = True
+    print('Total: '+str(n))
+    print('Merging Points')
+    while merged:
+        merged = False
+        n = 0
+        for i in range(len(particles)-1):
+            pset1 = particles[i]
+            pset2 = particles[i + 1]
+            dt = times[i + 1] - times[i]
+            didMerge, num = merge(pset1, pset2, dt)
+            n = n + num
+            merged = merged or didMerge
+        for i in reversed(range(1, len(particles))):
+            pset1 = particles[i]
+            pset2 = particles[i - 1]
+            dt = times[i - 1] - times[i]
+            didMerge, num = merge(pset1, pset2, dt)
+            n = n + num
+            merged = merged or didMerge
+        print('Merged '+str(n))
+        print('Number Per Frame: '+str(len(particles[0])))
+    print('Finished Merging')
+    xyz = XYZ()
+    for i in range(len(times)):
+        time = times[i]
+        pset = particles[i]
+        xyz_single = xyzFromParticles(time, pset)
+        xyz.xyzs.append(xyz_single)
+    return xyz
+
+
+if __name__ == '__main__':
+    xyz = XYZ()
+    xyz.load('sample.xyz')
+    xyz = process(xyz)
+    xyz.save('sample_mod.xyz')
