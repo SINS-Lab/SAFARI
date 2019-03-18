c776176215a38a8a86e99aaf64de441d2efe7496
Patrick <prjohns@g.clemson.edu>
2019-03-18 16:48:03 -0400

    Update helpers.py
    bit of visual cleanup.

--- misc/helpers.py
+++ misc/helpers.py
@@ -7,9 +7,16 @@ def rotate(a, b):
     b = b/np.linalg.norm(b)
 
     v = np.cross(a, b)
-    s = np.linalg.norm(v)
     c = a.dot(b)
     c = 1/(1+c)
     I = matlib.eye(3)
-    V = matlib.matrix([[0, -v[2], v[1]],[v[2],0,-v[0]],[-v[1],v[0],0]])
+    V = matlib.matrix([[0    ,-v[2], v[1]],\
+                       [v[2] ,0    ,-v[0]],\
+                       [-v[1],v[0] ,0    ]])
     return I + V + V.dot(V) * c
+
+def rotate_all(axis, dir, set):
+    # Ensure are normalized first.
+    axis = axis/np.linalg.norm(axis)
+    dir = dir/np.linalg.norm(dir)
+    
