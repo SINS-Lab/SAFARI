b92d2be0d0bdc909ae23e74705e60285b28fe0db
Patrick <prjohns@g.clemson.edu>
2019-03-21 22:58:33 -0400

    Update run_all.py
    Allows specifiying subdirectories to run from

--- gui/run_all.py
+++ gui/run_all.py
@@ -5,11 +5,17 @@ import time
 def run(threads, directory='.'):
     processes = []
     number = 1
+    
+    if directory!='.':
+        directory = os.path.join('.',directory)
+    
     for filename in os.listdir(directory):
         if filename.endswith(".input") and not filename==('safari.input'):
+            file = os.path.join(directory, filename).replace('.input','')
             saf_file = open('safari.input', 'w')
-            saf_file.write(filename.replace('.input', ''))
+            saf_file.write(file)
             saf_file.close()
+            print('Running: '+filename)
             process = subprocess.Popen('Safari.exe', shell=True)
             processes.append(process)
             #Wait a second for it to start running
@@ -18,7 +24,7 @@ def run(threads, directory='.'):
             # If hit number to run, wait for all runs to finish, before continuing.
             if number > threads:
                 for p in processes:
-                    p.join()
+                    p.wait()
                 processes = []
                 number = 1
 if __name__ == '__main__':
