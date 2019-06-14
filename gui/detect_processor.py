2fbeac9ceba12b94d72ce1daa29fa4e2fa7e1725
msdrigg <m.scott.driggers@gmail.com>
2019-06-14 01:06:51 -0400

    Added color by velocity (untested)

--- gui/detect_processor.py
+++ gui/detect_processor.py
@@ -20,6 +20,9 @@ import safari_input
 import subprocess
 import xyz_postprocess as xyz_p
 
+# Used for shift-click functionality
+shift_is_held = False
+
 def read(f, first, data):
     if first:
         #Emin EMax ESize ASize
@@ -268,10 +271,10 @@ class Detector:
                 fig.savefig('energyplot.png')
         return energy, intensity
         
-    def impactParam(self, basis = None, dx=0, dy=0):
-        x = self.detections[...,0]
-        y = self.detections[...,1]
-        c = self.detections[...,3]
+    def impactParam(self, basis=None, dx=0, dy=0):
+        x = self.detections[..., 0]
+        y = self.detections[..., 1]
+        c = self.detections[..., 3]
         
         fig, ax = plt.subplots()
         patches = []
@@ -297,10 +300,6 @@ class Detector:
                         colours.append(site[2])
                         circle = Circle((site[0]+i*dx, site[1]+j*dy), 1)
                         patches.append(circle)
-
-#        #TODO better colouring.
-#        for i in range(len(site)):
-#            site[i] = site[i] - minz
 
         p = PatchCollection(patches, alpha=0.4)
         p.set_array(np.array(colours))
@@ -341,8 +340,8 @@ class Detector:
                     close[1] = y[i]
                     index = i
 
-            if event.dblclick and event.button.value != 3:
-                #Setup a single run safari for this.
+            if event.dblclick and event.button.value == 1 and not shift_is_held:
+                # Setup a single run safari for this.
                 self.safio.fileIn = self.safio.fileIn.replace('_mod.input', '_ss.input')
                 self.safio.setGridScat(True)
                 self.safio.NUMCHA = 1
@@ -358,13 +357,13 @@ class Detector:
                 close[0] = round(close[0],2)
                 close[1] = round(close[1],2)
                 name = self.safio.fileIn.replace('.input', '')
-                xyz_p.process_file(name +'.xyz',\
+                xyz_p.process_file(name + '.xyz',
                                    name+str(close[0])+','+str(close[1])+'.xyz', load_vmd=True)
                 print(sub)
 
             if event.dblclick and event.button.value == 3:
-                #Setup a single run safari using colored data
-                print("Setting up a safari run for a colored dataset")
+                # Setup a single run safari using nearness colored data
+                print("Setting up a safari run for a nearness colored dataset")
                 self.safio.fileIn = self.safio.fileIn.replace('_mod.input', '_ss.input')
                 self.safio.setGridScat(True)
                 self.safio.NUMCHA = 1
@@ -380,18 +379,52 @@ class Detector:
                 close[0] = round(close[0], 2)
                 close[1] = round(close[1], 2)
                 name = self.safio.fileIn.replace('.input', '')
-                xyz_p.process_file(name +'.xyz',\
-                                   name+str(close[0])+','+str(close[1])+'.xyz', color_nearest_neighbors=True, load_vmd=True)
+                xyz_p.process_file(name +'.xyz',
+                                   name+str(close[0])+','+str(close[1])+'.xyz', color="nearest", load_vmd=True)
                 print(sub)
 
-            close[0] = round(close[0],5)
-            close[1] = round(close[1],5)
-            energy = round(self.detections[index][3],2)
+            if event.dblclick and event.button.value == 1 and shift_is_held:
+                # Setup a single run safari using velocity colored data
+                print("Setting up a safari run for a velocity colored dataset")
+                self.safio.fileIn = self.safio.fileIn.replace('_mod.input', '_ss.input')
+                self.safio.setGridScat(True)
+                self.safio.NUMCHA = 1
+                self.safio.XSTART = close[0]
+                self.safio.YSTART = close[1]
+                self.safio.genInputFile(fileIn=self.safio.fileIn)
+
+                command = 'Safari.exe'
+                if platform.system() == 'Linux':
+                    command = './Safari'
+
+                sub = subprocess.run(command, shell=True)
+                close[0] = round(close[0], 2)
+                close[1] = round(close[1], 2)
+                name = self.safio.fileIn.replace('.input', '')
+                xyz_p.process_file(name + '.xyz',
+                                   name+str(close[0])+','+str(close[1])+'.xyz', color="velocity", load_vmd=True)
+                print(sub)
+
+            close[0] = round(close[0], 5)
+            close[1] = round(close[1], 5)
+            energy = round(self.detections[index][3], 2)
             text.set_text(str(close)+', '+str(energy)+'eV')
             self.p.set_xdata([close[0]])
             self.p.set_ydata([close[1]])
             fig.canvas.draw()
 
+        def on_key_press(event):
+            if event.key == 'shift':
+                global shift_is_held
+                shift_is_held = True
+
+        def on_key_release(event):
+            if event.key == 'shift':
+                global shift_is_held
+                shift_is_held = False
+
+        fig.canvas.mpl_connect('key_press_event', on_key_press)
+        fig.canvas.mpl_connect('key_release_event', on_key_release)
         fig.canvas.mpl_connect('button_press_event', onclick)
         
         fig.show()
--- gui/xyz_postprocess.py
+++ gui/xyz_postprocess.py
@@ -2,7 +2,9 @@ from xyz import XYZ
 from xyz import XYZ_Single
 import subprocess
 import os
-import time
+import time as T
+import numpy as np
+import math
 
 
 class Particle:
@@ -27,10 +29,10 @@ class Particle:
                          value[5]/value[6]]
 
 
-def isSame(p_a, p_b, dt, color_nearest_neighbors=False):
+def isSame(p_a, p_b, dt, color=""):
     if p_a == p_b:
         return True
-    if p_a.atom != p_b.atom and not color_nearest_neighbors:
+    if p_a.atom != p_b.atom and color == "":
         return False
     # always the same for the projectile Atom
     if p_a.id == 0 and p_b.id == 0:
@@ -50,7 +52,7 @@ def isSame(p_a, p_b, dt, color_nearest_neighbors=False):
     return diff <= .25
 
 
-def merge(pset1, pset2, dt, first, color_nearest_neighbors=False):
+def merge(pset1, pset2, dt, first, color=""):
     len1 = len(pset1)
     len2 = len(pset2)
     merged = False
@@ -60,7 +62,7 @@ def merge(pset1, pset2, dt, first, color_nearest_neighbors=False):
         has = False
         for j in range(len2):
             p_b = pset2[j]
-            if isSame(p_a, p_b, dt, color_nearest_neighbors=color_nearest_neighbors):
+            if isSame(p_a, p_b, dt, color=color):
                 has = True
                 if not first:
                     break
@@ -73,7 +75,7 @@ def merge(pset1, pset2, dt, first, color_nearest_neighbors=False):
                     Particle.nextid = Particle.nextid + 1
 
         if not has:
-            if color_nearest_neighbors:
+            if color == "nearest":
                 p_a.atom = "X"
             pset2.append(p_a)
             merged = True
@@ -179,16 +181,10 @@ def smooth_framerate(times, particles):
     print("Begining smoothing")
     # get minimum timestep
     minimum_timestep = times[-1]
-    minimum_index = 0
     for index in range(20, len(times) - 1):
         timestep = times[index + 1] - times[index]
         if timestep < minimum_timestep:
             minimum_timestep = timestep
-            minimum_index = index
-    for index, item in enumerate(particles):
-        if len(item) != len(particles[0]):
-            print(len(item))
-            print(index)
     minimum_timestep = max(minimum_timestep, times[-1]/1000)
     new_times_array = []
     new_particles_array = []
@@ -200,8 +196,22 @@ def smooth_framerate(times, particles):
     print("Smoothing Done")
     return new_times_array, new_particles_array
 
+def get_velocity_parameters(particles):
+    helper_sum = 0
+    for state in particles:
+        for particle in state:
+            if particle.id != 0:
+                helper_sum += np.linalg.norm(particle.velocity)
+    average = helper_sum/(len(particles)*len(particles[0] - 1))
+    helper_sum = 0
+    for state in particles:
+        for particle in state:
+            if particle.id != 0:
+                helper_sum += (average - np.linalg.norm(particle.velocity))**2
+    variation_average = helper_sum/(len(particles)*len(particles[0] - 1))
+    return average, math.sqrt(variation_average)
 
-def process(xyz, color_nearest_neighbors=False):
+def process(xyz, color=""):
     # Array of elapsed times
     times = []
     # This is an array of arrays of Particles, which correspond to times
@@ -230,7 +240,7 @@ def process(xyz, color_nearest_neighbors=False):
             pset1 = particles[i]
             pset2 = particles[i + 1]
             dt = times[i + 1] - times[i]
-            didMerge, num = merge(pset1, pset2, dt, True, color_nearest_neighbors=color_nearest_neighbors)
+            didMerge, num = merge(pset1, pset2, dt, True, color=color)
             n = n + num
             merged = merged or didMerge
         if n == 0:
@@ -241,7 +251,7 @@ def process(xyz, color_nearest_neighbors=False):
             pset1 = particles[i]
             pset2 = particles[i - 1]
             dt = times[i - 1] - times[i]
-            didMerge, num = merge(pset1, pset2, dt, False, color_nearest_neighbors=color_nearest_neighbors)
+            didMerge, num = merge(pset1, pset2, dt, False, color=color)
             n = n + num
             merged = merged or didMerge
         print('Merged '+str(n))
@@ -249,9 +259,16 @@ def process(xyz, color_nearest_neighbors=False):
     print('Finished Merging')
 
     for pset in particles:
-        def key(p):
-            return p.id
-        pset.sort(key=key)
+        # I put a lambda function in here
+        pset.sort(key=lambda p: p.id)
+
+    if color == "velocity":
+        mean, std = get_velocity_parameters(particles)
+        for state in particles:
+            for atom in state:
+                if np.linalg.norm(atom.velocity) - mean > std and atom.id != 0:
+                    atom.atom = "X"
+
     # Smooth the framerate
     times, particles = smooth_framerate(times, particles)
     xyz = XYZ()
@@ -263,28 +280,33 @@ def process(xyz, color_nearest_neighbors=False):
     return xyz
 
 
-def process_file(fileIn, fileOut=None, color_nearest_neighbors=False, load_vmd=False):
+def process_file(fileIn, fileOut=None, color="", load_vmd=False):
+    """
+    :param color: string describing the coloring pattern. "" for uniform coloring, "nearest" for coloring of
+        nearest 10 atoms, and "momentum" to color the atoms that receive additional momentum differently
+    :param load_vmd: boolean describing whether to open vmd upon completetion (true) or not (false)
+    """
     if fileOut is None:
         fileOut = fileIn
     xyz = XYZ()
     xyz.load(fileIn)
-    xyz = process(xyz, color_nearest_neighbors=color_nearest_neighbors)
+    xyz = process(xyz, color=color)
     if color_nearest_neighbors:
         fileOut = fileOut[:-4] + "COLORED.xyz"
     xyz.save(fileOut)
     if load_vmd:
         # MAKE THE FILENAME INCLUDE DIRECTORY
-        if color_nearest_neighbors:
+        if color != "":
             commands = ["topo readvarxyz {}\n".format(fileOut)]
         else:
             commands = ["mol new {}\n".format(fileOut)]
         commands.append("mol modstyle 0 0 \"VDW\"")
-        try :
+        try:
             with open("commands.vmd", "w") as file:
                 file.writelines(commands)
             subprocess.Popen("vmd -e commands.vmd")
         finally:
-            time.sleep(5)
+            T.sleep(5)
             os.remove("commands.vmd")
 
 
